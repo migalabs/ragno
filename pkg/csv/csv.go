91c88154889771e2398c2684385207ae21f04928
0x00Duke <leon.ducasse@epitech.eu>
2023-06-20 18:51:58 +0200

    [MDF/ADD] peerDiscovorers to get the nodes we want to connect to, csv file or discovering

--- pkg/csv/csv.go
+++ pkg/csv/csv.go
@@ -1,4 +1,4 @@
-package crawler
+package csv
 
 import (
 	"encoding/csv"
---
+++ pkg/utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import "github.com/ethereum/go-ethereum/p2p/enode"
+
+func ParseStringToEnr(enr string) *enode.Node {
+	// parse the Enr
+	remoteEnr, err := enode.Parse(enode.ValidSchemes, enr)
+	if err != nil {
+		remoteEnr = enode.MustParseV4(enr)
+	}
+	return remoteEnr
+}
