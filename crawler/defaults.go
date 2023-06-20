27a5f1c856fb785c11e828dfa27d8bddc9bac260
0x00Duke <leon.ducasse@epitech.eu>
2023-06-20 11:51:50 +0200

    [MDF] put amount of concurrent workers in a var in crawler obj & change names

--- crawler/defaults.go
+++ crawler/defaults.go
@@ -4,14 +4,14 @@ import ()
 
 var (
 	// crawler host related metrics
-	DefaultLogLevel    = "info"
-	DefaultDBEndpoint  = "postgresql://user:password@localhost:5432/ragno"
-	DefaultHostIP      = "0.0.0.0"
-	DefaultHostPort    = 9050
-	DefaultMetricsIP   = "localhost"
-	DefaultMetricsPort = 9070
-	DefaultWorkerNum   = 150
-	DefaultSaverNum    = 150
+	DefaultLogLevel          = "info"
+	DefaultDBEndpoint        = "postgresql://user:password@localhost:5432/ragno"
+	DefaultHostIP            = "0.0.0.0"
+	DefaultHostPort          = 9050
+	DefaultMetricsIP         = "localhost"
+	DefaultMetricsPort       = 9070
+	DefaultConcurrentDialers = 150
+	DefaultConcurrentSavers  = 150
 
 	// Not using yaml files so far
 	DefaultConfigFile = "config/example.yaml"
