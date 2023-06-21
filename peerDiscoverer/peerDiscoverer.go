a07755750a70840694041d9daaeb520b13971bbd
0x00Duke <leon.ducasse@epitech.eu>
2023-06-21 16:34:56 +0200

    [MDF] dirrectly create the right discoverer, and pass the channel to the run method

--- peerDiscoverer/peerDiscoverer.go
+++ peerDiscoverer/peerDiscoverer.go
@@ -1,45 +1,14 @@
 package peerDiscoverer
 
 import (
-	"context"
-
 	"github.com/cortze/ragno/modules"
-	"github.com/sirupsen/logrus"
 )
 
 type PeerDiscoverer interface {
 	// Run starts the peer discovery process or get the nodes from the file
-	Run() error
-	// Channel returns the channel where the nodes are sent
-	Channel() chan *modules.ELNode
+	Run(sendingChan chan *modules.ELNode) error
 	// sendNodes sends the nodes to the channel
-	sendNodes(node *modules.ELNode)
-}
-
-type PeerDiscovererConf struct {
-	Type        DiscovererType
-	SendingChan chan *modules.ELNode
-	File        string
-	Port        int
-}
-
-func NewPeerDiscoverer(ctx context.Context, conf PeerDiscovererConf) (PeerDiscoverer, error) {
-	switch conf.Type {
-	case CsvType:
-		logrus.Info("Using CSV peer discoverer")
-		return NewCSVPeerDiscoverer(conf)
-	case Discv4Type:
-		logrus.Info("Using Discv4 peer discoverer")
-		return NewDisv4PeerDiscoverer(conf)
-	default:
-		logrus.Error("Unknown peer discoverer type")
-		return nil, nil
-	}
+	sendNodes(sendingChan chan *modules.ELNode, node *modules.ELNode)
 }
 
 type DiscovererType int
-
-const (
-	Discv4Type DiscovererType = iota
-	CsvType
-)
