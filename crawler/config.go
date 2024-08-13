55c639b5fe91ad91ab6a3e6cde775996af9ee736
yuliannabn <yulianna.barahona-naranjo@epitech.eu>
2024-08-13 02:42:04 +0000

    fix: change defaults to default values

--- crawler/config.go
+++ crawler/config.go
@@ -10,11 +10,11 @@ import (
 var (
 	// crawler host related metrics
 	DefaultLogLevel             = "info"
-	DefaultDBEndpoint           = "postgresql://user:password@localhost:5440/ragnodb?sslmode=disable"
+	DefaultDBEndpoint           = "postgresql://user:password@localhost:5432/ragnodb"
 	DefaultHostIP               = "0.0.0.0"
-	DefaultHostPort             = 9051
+	DefaultHostPort             = 9050
 	DefaultMetricsIP            = "localhost"
-	DefaultMetricsPort          = 9071
+	DefaultMetricsPort          = 9070
 	DefaultMetricsEndpoint      = "metrics"
 	DefaultConcurrentDialers    = 150
 	DefaultConcurrentPersisters = 2
